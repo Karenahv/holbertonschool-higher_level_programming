20bf05053c69bf96923e74f6403562d16131860a
Karen Herrera <837@holbertonschool.com>
2019-09-10 21:09:40 +0000

    task #13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* insert_node - inserts a new node at a given position
+* @head: first element of the list
+* @number: index of the node we want to insert
+* Return: the value of a n node.
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+
+	listint_t *current = NULL;
+	int idx = 0;
+	struct listint_s *new = NULL;
+
+
+	current = *head;
+	while (current != NULL)
+	{
+
+		if (number > current->n)
+		{
+			current = current->next;
+			idx++;
+		}
+		else
+		{
+			new = insert_nodeint_at_index(head, idx, number);
+			break;
+		}
+	}
+	return (new);
+}
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: first element of the list
+ * @idx: index of the node we want to insert
+ * @n: value of the node
+ * Return: the value of a n node.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	struct listint_s *temp = NULL;
+	struct listint_s *new = NULL;
+	int count;
+
+	count = 0;
+	i = 0;
+	temp = (struct listint_s *)malloc(sizeof(struct listint_s));
+	new = (struct listint_s *)malloc(sizeof(struct listint_s));
+	if (new == NULL || head == NULL || temp == NULL)
+		return (NULL);
+	if (idx == 0)
+	{
+		temp->n = n;
+		temp->next = *head;
+		*head = temp;
+		return (temp);
+	}
+	if (*head == 0)
+		return (NULL);
+	temp = *head;
+	new->n = n;
+	while (temp != NULL)
+	{
+		if (temp == NULL)
+			return (NULL);
+		if (i == (idx - 1))
+		{
+			new->next = temp->next;
+			temp->next = new;
+			count = 1;
+			break;
+		}
+		i++;
+		temp = temp->next;
+	}
+	if (count == 0)
+		return (NULL);
+	return (new);
+}

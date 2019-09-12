2671a438514bd40ac42823f187f817a21d4b153d
Karen Herrera <837@holbertonschool.com>
2019-09-12 21:21:36 +0000

    task #6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    numofargs = len(sys.argv)
+    listargs = sys.argv
+    list1 = ['+', '-', '*', '/']
+    list2 = [add, sub, mul, div]
+    i = 0
+    a = int(listargs[1])
+    b = int(listargs[3])
+    flag = 0
+
+    if numofargs != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    for operator in list1:
+        if operator == listargs[2]:
+            print("{} {} {} = {}".format(a, list1[i], b, list2[i](a, b)))
+            flag = 1
+        i = i + 1
+    if flag == 0:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)

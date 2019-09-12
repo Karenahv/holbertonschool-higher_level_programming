3bf6d22a49986c76e6098c2e6c51b4e025ff3e34
Karen Herrera <837@holbertonschool.com>
2019-09-12 16:59:36 +0000

    task #2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+numofargs = len(sys.argv)
+listargs = sys.argv
+if numofargs == 1:
+    print("{} arguments.".format(numofargs - 1))
+elif numofargs == 2:
+    print("{} argument:".format(numofargs - 1))
+else:
+    print("{} arguments:".format(numofargs - 1))
+for i in range(1, numofargs):
+    print("{}: {}".format(i, listargs[i]))

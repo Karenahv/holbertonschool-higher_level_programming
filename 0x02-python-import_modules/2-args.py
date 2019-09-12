98192b7e0ab8aa666fb998a388a40068048c0ce6
Karen Herrera <837@holbertonschool.com>
2019-09-12 17:12:21 +0000

    task #2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(numofargs - 1))
     elif numofargs == 2:
         print("{} argument:".format(numofargs - 1))
+        print("{}: {}".format(1, listargs[1]))
     else:
         print("{} arguments:".format(numofargs - 1))
         for i in range(1, numofargs):

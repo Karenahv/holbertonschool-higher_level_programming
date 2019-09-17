ac403dc1d41f57c80247cfa825aa290924b1b785
Karen Herrera <837@holbertonschool.com>
2019-09-17 17:29:07 +0000

    task #5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 def no_c(my_string):
     new_string = ""
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            new_string += my_string[i]
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
     return (new_string)

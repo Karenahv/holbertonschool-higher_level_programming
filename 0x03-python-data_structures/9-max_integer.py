bcf8d9c344d584108cad87851aa22e0a594967c3
Karen Herrera <837@holbertonschool.com>
2019-09-17 19:26:02 +0000

    task #9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0 or my_list is None:
+    if len(my_list) == 0:
         return None
     if isinstance(my_list, list):
         if my_list[0] > my_list[1]:

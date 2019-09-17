42ef6175ae53e8b61faa7afbb64788b21f406c7f
Karen Herrera <837@holbertonschool.com>
2019-09-17 17:05:34 +0000

    task #1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,4 @@ def element_at(my_list, idx):
     sizeoflist = len(my_list)
     if idx > sizeoflist - 1 or idx < 0 or my_list is None or idx is None:
         return
-    if idx > 0:
-        return (my_list[idx])
-    return
+    return (my_list[idx])

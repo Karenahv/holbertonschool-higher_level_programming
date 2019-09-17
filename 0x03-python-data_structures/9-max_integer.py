6a3f74b0e659fdf366ad7ac11ab1bebedd9bb3ea
Karen Herrera <837@holbertonschool.com>
2019-09-17 19:32:14 +0000

    task #9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,11 +3,11 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     if isinstance(my_list, list):
-        if my_list[0] > my_list[1]:
+        if my_list[0] >= my_list[1]:
             maximo = my_list[0]
         else:
             maximo = my_list[1]
             for i in range(1, len(my_list)):
-                if my_list[i] > maximo:
+                if my_list[i] >= maximo:
                     maximo = my_list[i]
         return (maximo)

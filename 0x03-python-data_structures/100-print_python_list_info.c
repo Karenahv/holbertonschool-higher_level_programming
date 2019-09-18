be92a50ee03404d582fef1bd11c3adbc3b33bbb2
Karen Herrera <837@holbertonschool.com>
2019-09-18 01:41:24 +0000

    task #14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,18 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+
+{
+	int i;
+
+	printf("[*] Size of the Pytnon List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+       for (i = 0; i < Py_SIZE(p); i++)
+       {
+	       printf("Element %d: ", i);
+	       printf("%s\n", Py_TYPE(PyList_GetItem(p, i))->tp_name);
+       }
+
+
+}

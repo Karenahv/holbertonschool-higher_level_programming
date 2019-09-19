c8d070714126dcc7e0ca25dc04c003ddb55d6861
Karen Herrera <837@holbertonschool.com>
2019-09-19 17:11:10 +0000

    task #10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) <= 0:
         return None
     maximo = list(a_dictionary.values())[0]
     llave = list(a_dictionary.keys())[0]

b6b8cbaea2a848bdf14df174a6665ae8269d044c
Karen Herrera <837@holbertonschool.com>
2019-10-05 11:47:49 +0000

    4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,8 +3,14 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     for char in text:
-        if char == '.' or char == '?' or char == ':':
+        if char == '.':
+            print('.')
             print()
+        elif char == '?':
+            print('?')
+            print()
+        elif char == ':':
+            print(':')
             print()
         else:
             print("{}".format(char), end="")

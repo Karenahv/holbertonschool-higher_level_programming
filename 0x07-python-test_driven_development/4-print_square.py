5349925c394401dcf5ccae74e98a5eec208881bb
Karen Herrera <837@holbertonschool.com>
2019-10-05 11:36:07 +0000

    4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,8 +4,6 @@
 
 def print_square(size):
     """ print squares"""
-    if size == 0:
-        print()
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:

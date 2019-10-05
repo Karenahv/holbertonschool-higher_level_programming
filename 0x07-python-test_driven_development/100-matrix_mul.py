94389059bb53a46a88744a49ac86041d5165c88f
Karen Herrera <837@holbertonschool.com>
2019-10-05 11:58:01 +0000

    100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -18,24 +18,24 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
     lenmatrixa = len(m_a)
     lenrowa = len(m_a[0])
-    for i in range(lenmatrixa):
-        for j in range(lenrowa):
-            if type(m_a[i][j]) is not int and type(m_a[i][j]) is not\
-               float:
-                raise TypeError("m_a should contain only integers or floats")
     lenmatrixb = len(m_b)
     lenrowb = len(m_b[0])
-    for i in range(lenmatrixb):
-        for j in range(lenrowb):
-            if type(m_b[i][j]) is not int and type(m_b[i][j]) is not\
-               float:
-                raise TypeError("m_b should contain only integers or floats")
     for i in range(lenmatrixa):
         if len(m_a[i]) != lenrowa:
             raise TypeError("each row of m_a must be of the same size")
     for i in range(lenmatrixb):
         if len(m_b[i]) != lenrowb:
             raise TypeError("each row of m_b must be of the same size")
+    for i in range(lenmatrixa):
+        for j in range(lenrowa):
+            if type(m_a[i][j]) is not int and type(m_a[i][j]) is not\
+               float:
+                raise TypeError("m_a should contain only integers or floats")
+    for i in range(lenmatrixb):
+        for j in range(lenrowb):
+            if type(m_b[i][j]) is not int and type(m_b[i][j]) is not\
+               float:
+                raise TypeError("m_b should contain only integers or floats")
     if lenrowa != lenmatrixb:
         raise IndexError("m_a and m_b can't be multiplied")
     matrix = []

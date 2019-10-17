1b0e78188a18f6448709158b7eceeb3db822d747
Karen Herrera <837@holbertonschool.com>
2019-10-17 01:09:03 +0000

    14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""pascal triangle"""
+
+
+def pascal_triangle(n):
+    list1 = []
+    if n <= 0:
+        return list1
+    for i in range(n):
+        list1.append([])
+        list1[i].append(1)
+        for j in range(1, i):
+            list1[i].append(list1[i - 1][j - 1] + list1[i - 1][j])
+        if n != 0:
+            list1[i].append(1)
+    return list1

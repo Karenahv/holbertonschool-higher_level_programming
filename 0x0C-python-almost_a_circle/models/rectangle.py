d73d0e67cfc3a46dbf1f677074a453ce9e0fbe0b
Karen Herrera <837@holbertonschool.com>
2019-10-22 17:49:22 +0000

    more comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,10 +17,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """class rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """class rectangle"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -29,10 +31,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """class rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """class rectangle"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -41,10 +45,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """class rectangle"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """class rectangle"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -53,10 +59,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """class rectangle"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """class rectangle"""
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -64,9 +72,11 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """class rectangle"""
         return (self.__width * self.__height)
 
     def display(self):
+        """class rectangle"""
         for k in range(self.__y):
             print()
         for i in range(self.__height):
@@ -77,10 +87,12 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
+        """class rectangle"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
 
     def update(self, *args, **kwargs):
+        """class rectangle"""
         lenofargs = len(args)
         if lenofargs > 0:
             self.id = args[0]
@@ -110,6 +122,7 @@ class Rectangle(Base):
                     self.id = value
 
     def to_dictionary(self):
+        """class rectangle"""
         dic1 = {}
         for key, value in self.__dict__.items():
             if 'x' in key:

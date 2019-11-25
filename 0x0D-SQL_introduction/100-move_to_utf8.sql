4fc8ce891e797632e8dcee95d3a441e056aa23df
Karen Herrera <837@holbertonschool.com>
2019-11-25 23:41:28 +0000

    number 17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- convert table
+ALTER DATABASE  hbtn_0c_0  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE  hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

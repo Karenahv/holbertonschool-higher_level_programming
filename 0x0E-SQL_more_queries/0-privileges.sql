5b188d7dfc19dfed1175d40fe226f658b6cf0c58
Karen Herrera <837@holbertonschool.com>
2019-11-26 15:14:06 +0000

    first commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Show privileges user
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR  user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create User with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+fLUSH PRIVILEGES;

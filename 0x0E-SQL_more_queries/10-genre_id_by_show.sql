9f8bab4e7cfec3fb81ca6f89079d25578346a19d
Karen Herrera <837@holbertonschool.com>
2019-11-26 20:13:00 +0000

    number 10 again

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- select cities california
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows.tv_shows, hbtn_0d_tvshows.tv_show_genres
-WHERE tv_show_genres.genre_id = tv_shows.id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id 
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

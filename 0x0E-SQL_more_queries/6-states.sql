7c1c505401c1147a3eff8415e920e8c71e24bb5a
Karen Herrera <837@holbertonschool.com>
2019-11-26 15:56:10 +0000

    number 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Create tables
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT NOT NULL AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+       name VARCHAR(256) NOT NULL);

f40f47ce71524227a2077effcb8d733d1fb47de7
Karen Herrera <837@holbertonschool.com>
2019-11-27 00:22:42 +0000

    number 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- select cities california
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

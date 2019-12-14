db39c0748047372d34acb365ca2aeb424f8ac63a
Karen Herrera <837@holbertonschool.com>
2019-12-14 00:08:56 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists all states from hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("select * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,7 @@
+# 0x0F. Python - Object-relational mapping
+## Learning objectives:
+- How to connect to a MySQL database from a Python script
+- How to SELECT rows in a MySQL table from a Python script
+- How to INSERT rows in a MySQL table from a Python script
+- What ORM means
+- How to map a Python Class to a MySQL table

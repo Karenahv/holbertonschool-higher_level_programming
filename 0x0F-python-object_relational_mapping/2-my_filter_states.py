274a2e6aca9b3b56a3facfa57a9e578a5a84516b
Karen Herrera <837@holbertonschool.com>
2019-12-14 17:42:55 +0000

    number 2 with format function

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("select * FROM states WHERE BINARY name = %s ORDER BY id",
-                (argv[4],))
+    q = """
+    SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    q = q.format(argv[4])
+    cur.execute(q)
     rows = cur.fetchall()
     for row in rows:
         print(row)

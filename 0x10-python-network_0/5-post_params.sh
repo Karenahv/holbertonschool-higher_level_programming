c03219a7606d111c9280075b2fd683f1acad20f2
Karen Herrera <837@holbertonschool.com>
2020-01-06 18:30:30 +0000

    number 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a uRL, method POST
-curl -sd "emaiL=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1" 
+curl -s -X POST -d "emaiL=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1" 

03e0ff00ec8aa74d15cc749c479cad1414491b2c
Karen Herrera <837@holbertonschool.com>
2020-01-13 15:08:57 +0000

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetch an url"""
+import urllib.request
+
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+print("Body response:\n\t-type: {}\n\t-content: {}\n\t-utf8 content: {}"
+      .format(type(html), html, html.decode('utf-8')))

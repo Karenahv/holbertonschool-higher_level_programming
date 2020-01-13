d7eb6e46cb694a098910a509a59748cc25f2c5be
Karen Herrera <837@holbertonschool.com>
2020-01-13 19:28:19 +0000

    number 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" sends and url and displays value header with requests"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    listargs = sys.argv
+    req = requests.get(listargs[1])
+    print(req.headers['X-Request-Id'])

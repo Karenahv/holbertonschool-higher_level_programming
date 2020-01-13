69c9dc6ae68b7c38a58b8349c3d656055eeb0db7
Karen Herrera <837@holbertonschool.com>
2020-01-13 21:03:28 +0000

    nuner 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" sends a POST"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        listargs = sys.argv
+        values = {'q': listargs[1]}
+    else:
+        data = {}
+    try:
+        req = requests.post(url, data=values)
+        data1 = req.json()
+        if not data1:
+            print("No result")
+        else:
+            print("[<{}>] <{}>".format(data1.get('id'), data1.get('name')))
+    except ValueError:
+        print("Not a valid JSON")

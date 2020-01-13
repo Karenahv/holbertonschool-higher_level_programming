6f99b0eee1a3121b52a11392eda8d223a8a1215d
Karen Herrera <837@holbertonschool.com>
2020-01-13 19:06:55 +0000

    number 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" sends and url and displays value"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+        try:
+                listargs = sys.argv
+                req = urllib.request.Request(listargs[1])
+                with urllib.request.urlopen(req) as response:
+                        html = response.read()
+                print(html.decode('utf-8'))
+        except urllib.error.HTTPError as e:
+                print('Error Code: {}'.format(e.code))

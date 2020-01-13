46f70ebd6a37a76abeb5270894cb53000fe2d32c
Karen Herrera <837@holbertonschool.com>
2020-01-13 18:24:57 +0000

    number 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ sends and url and displays value"""
 import urllib.request
-import urllib
 import sys
 
-
-listargs = sys.argv
-req = urllib.request.Request(listargs[1])
-with urllib.request.urlopen(req) as response:
-    html = response.info()
-print(html['X-Request-Id'])
+if __name__ == "__main__":
+    listargs = sys.argv
+    req = urllib.request.Request(listargs[1])
+    with urllib.request.urlopen(req) as response:
+        html = response.info()
+    print(html['X-Request-Id'])

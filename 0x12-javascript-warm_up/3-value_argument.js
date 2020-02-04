0c695b043a01b4bfd7a59ea65ac2fe877401748a
Karen Herrera <837@holbertonschool.com>
2020-02-04 17:20:52 +0000

    number 3 correction

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,2 @@
 #!/usr/bin/node
-let n;
-if (process.argv.length <= 2) {
-  n = 'No argument';
-} else if (process.argv.length === 3) {
-  n = process.argv[2];
-} else {
-  n = process.argv[2];
-}
-console.log(n);
+console.log(process.argv[2] || 'No argument');

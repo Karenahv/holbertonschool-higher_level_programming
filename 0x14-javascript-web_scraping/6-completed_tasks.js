ed92cb9c170b661184537a0b2124afbe62e00898
Karen Herrera <837@holbertonschool.com>
2020-02-11 18:08:17 +0000

    number 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// status of a request
+const myRequest = require('request');
+const myUrl = process.argv[2];
+myRequest(myUrl, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let count = 0;
+    const dict1 = {};
+    const jsonBody = JSON.parse(body);
+    let userant = jsonBody[0].userId;
+    for (let j = 1; j < jsonBody.length; j++) {
+      for (let i = 0; i < jsonBody.length; i++) {
+        const user = jsonBody[i].userId;
+        if (jsonBody[i].completed === true && userant === user) {
+          count = count + 1;
+          dict1[jsonBody[i].userId] = count;
+        }
+      }
+      userant = jsonBody[j].userId;
+      count = 0;
+    }
+    console.log(dict1);
+  }
+});

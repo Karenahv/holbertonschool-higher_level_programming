0b7101d0269592659a1d0bfd439dd0e762e5d6e3
Karen Herrera <837@holbertonschool.com>
2020-02-11 17:16:51 +0000

    number 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// status of a request
+const myRequest = require('request');
+const myUrl = process.argv[2];
+myRequest(myUrl, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let count = 0;
+    const jsonBody = JSON.parse(body).results;
+    for (let i = 0; i < jsonBody.length; i++) {
+      const chars = jsonBody[i].characters;
+      for (let j = 0; j < chars.length; j++) {
+        if (chars[j] === 'https://swapi.co/api/people/18/') {
+          count = count + 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});

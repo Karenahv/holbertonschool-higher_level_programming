903e9f70f4bf564fab8ae87b7d17afcfbfc4568e
Karen Herrera <837@holbertonschool.com>
2020-02-12 13:53:46 +0000

    number 6 fix json empty

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,19 +9,19 @@ myRequest(myUrl, function (err, res, body) {
     let count = 0;
     const dict1 = {};
     const jsonBody = JSON.parse(body);
-    let userant = jsonBody[0].userId;
-    for (let j = 1; j < jsonBody.length; j++) {
-      for (let i = 0; i < jsonBody.length; i++) {
-        const user = jsonBody[i].userId;
-        if (jsonBody[i].completed === true && userant === user) {
-          count = count + 1;
-          if (count > 0) {
+    if (jsonBody) {
+      let userant = jsonBody[0].userId;
+      for (let j = 1; j < jsonBody.length; j++) {
+        for (let i = 0; i < jsonBody.length; i++) {
+          const user = jsonBody[i].userId;
+          if (jsonBody[i].completed === true && userant === user) {
+            count = count + 1;
             dict1[jsonBody[i].userId] = count;
           }
         }
+        userant = jsonBody[j].userId;
+        count = 0;
       }
-      userant = jsonBody[j].userId;
-      count = 0;
     }
     console.log(dict1);
   }
